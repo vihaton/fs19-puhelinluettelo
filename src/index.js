23eff39dc92570bfac71427210f70c730c63c7c5
Vili <vili.hatonen@helsinki.fi>
2019-02-06 21:20:35 +0200

    added cors, init procfile

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
+const cors = require('cors')
 
 morgan.token('person', function getPerson (req) {
     str = JSON.stringify(req.body)
@@ -9,9 +10,10 @@ morgan.token('person', function getPerson (req) {
         return ""
     }
     return JSON.stringify(req.body)
-  })
-  
+})
 
+
+app.use(cors())
 app.use(bodyParser.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :person'))
 
@@ -110,7 +112,7 @@ app.get("/info", (req, res) => {
             </div>`)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
